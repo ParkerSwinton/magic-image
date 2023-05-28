8fea2e62a56b9d7ab4f577fb0a6151b66599363d
ParkerSwinton <parkerdswinton@gmail.com>
2023-05-28 12:12:35 -0700

    Responsive Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,33 +3,75 @@ import { Pacifico } from 'next/font/google'
 const pacifico = Pacifico({ subsets: ['cyrillic'], weight: '400' })
 
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
-    <nav className='fixed w-full'>
-      <div className='flex h-14 items-center justify-between p-4'>
-        <Link className={`text-3xl ${pacifico.className}`} href='/'>
-          Magic<span className='pl-0.5 text-sky-500'>Image</span>
-        </Link>
-        <ul className='flex items-center gap-10 text-lg text-gray-400'>
-          <Link className='hover:text-sky-500' href='/'>
+    <header className='fixed w-full'>
+      <div className='divide-y divide-zinc-800 bg-zinc-900 sm:flex sm:items-center sm:justify-between sm:divide-y-0 sm:px-4 sm:py-3'>
+        <div className='flex items-center justify-between  px-4 py-3 sm:p-0'>
+          <Link
+            className={`text-2xl ${pacifico.className} sm:text-3xl`}
+            href='/'
+          >
+            Magic<span className='pl-0.5 text-sky-500'>Image</span>
+          </Link>
+          <button
+            type='button'
+            className='text-zinc-50 hover:text-sky-500 focus:text-sky-500  sm:hidden'
+            onClick={() => setOpen((curr) => !curr)}
+          >
+            <svg viewBox='0 0 24 24' className='h-6 w-6 fill-current'>
+              <path
+                visibility={open ? 'visible' : 'hidden'}
+                fill-rule='evenodd'
+                d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'
+              />
+              <path
+                visibility={!open ? 'visible' : 'hidden'}
+                fill-rule='evenodd'
+                d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'
+              />
+            </svg>
+          </button>
+        </div>
+        <nav
+          className={`${
+            open ? 'flex' : 'hidden'
+          } flex-col items-center text-lg text-gray-400 sm:flex sm:flex-row sm:gap-10`}
+        >
+          <Link
+            className='w-full py-1 text-center hover:bg-zinc-800 hover:text-sky-500 sm:hover:bg-inherit'
+            href='/'
+            onClick={() => setOpen(false)}
+          >
             HOME
           </Link>
-          <Link className='hover:text-sky-500' href='/services'>
+
+          <Link
+            className='w-full py-1 text-center hover:bg-zinc-800 hover:text-sky-500 sm:hover:bg-inherit'
+            href='/services'
+            onClick={() => setOpen(false)}
+          >
             SERVICES
           </Link>
-          <Link className='hover:text-sky-500' href='/about'>
+          <Link
+            className='w-full  py-1 text-center hover:bg-zinc-800 hover:text-sky-500 sm:hover:bg-inherit'
+            href='/about'
+            onClick={() => setOpen(false)}
+          >
             ABOUT
           </Link>
           <Link
-            className='rounded bg-sky-500 px-3 font-bold text-zinc-50 transition hover:bg-zinc-50 hover:text-sky-500'
-            href='contact'
+            className='w-full rounded bg-sky-500 px-3 text-center font-bold text-zinc-50 transition hover:bg-zinc-50 hover:text-sky-500'
+            href='/contact'
+            onClick={() => setOpen(false)}
           >
             CONTACT
           </Link>
-        </ul>
+        </nav>
       </div>
-    </nav>
+    </header>
   )
 }
