6e33175449ef9a6e99cd81296d3a86ab811a9eb5
ParkerSwinton <parkerdswinton@gmail.com>
2023-06-04 12:19:54 -0700

    Move faq data into utils

---
+++ src/utils/faq.ts
@@ -0,0 +1,16 @@
+export const faqData = [
+  {
+    question: 'How does mobile detailing work?',
+    answer:
+      'Mobile detailing means we can come to you! All we need is a water and power source withing 80 feet of your vehicle.',
+  },
+  {
+    question: 'Can I bring my car to you?',
+    answer: 'Yes! You can drop your car off and leave the rest to us! (North Saanich)',
+  },
+  {
+    question: 'Why should I choose you?',
+    answer:
+      'Being driven by passion and with such little overhead we can provide a professional service for a fraction of the cost. We are also incredibly flexible and can accommodate your needs!',
+  },
+]
