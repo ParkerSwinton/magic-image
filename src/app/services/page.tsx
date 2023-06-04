53b1ab7c42f867222544ed8c804629370a86f40a
ParkerSwinton <parkerdswinton@gmail.com>
2023-06-04 10:12:53 -0700

    Fix max-w on larger screens

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,22 +4,18 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between p-12'>
       <h1 className='py-6 text-3xl font-extrabold'>Services</h1>
-      <div className='flex flex-col  space-y-6 divide-y divide-zinc-100/20'>
+      <div className='flex max-w-md  flex-col space-y-6 divide-y divide-zinc-100/20'>
         <div className='space-y-4'>
           <p className='text-zinc-400'>
             <span className='pr-1 font-bold text-zinc-50'>Exterior Only: </span>
-            Hand wash and dry with a cleaning of all inner windows. Finished
-            with protectant for paint, tires and rims.
+            Hand wash and dry with a cleaning of all inner windows. Finished with protectant for paint, tires and rims.
           </p>
           <p className='text-zinc-400'>
             <span className='pr-1 font-bold text-zinc-50'>Interior Only: </span>
-            Deep clean of all surfaces including windows and door jams. Finished
-            with protectant for surfaces.
+            Deep clean of all surfaces including windows and door jams. Finished with protectant for surfaces.
           </p>
           <p className='text-zinc-400'>
-            <span className='pr-1 font-bold text-zinc-50'>
-              Premium Detail:{' '}
-            </span>
+            <span className='pr-1 font-bold text-zinc-50'>Premium Detail: </span>
             The best of both worlds.
           </p>
         </div>
