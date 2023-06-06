9336ba31cd5349bad748b55e55a9d75282d136e9
ParkerSwinton <parkerdswinton@gmail.com>
2023-06-05 20:10:16 -0700

    Add text to mobile home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex min-h-screen flex-col space-y-3 px-10 pt-20'>
+      <h1 className='text-4xl font-extrabold text-sky-500'>
+        Love your car <br />
+        like it's brand
+        <br />
+        new again.
+      </h1>
+      <h2 className='text-lg font-bold text-zinc-400'>
+        We provide high quality mobile
+        <br />
+        car detailing for a fraction of the
+        <br />
+        price.
+      </h2>
+    </main>
   )
 }
