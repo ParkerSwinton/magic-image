9dd549786f3ce4c17d5eddb71ac9d8d1610463be
ParkerSwinton <parkerdswinton@gmail.com>
2023-06-10 15:23:45 -0700

    Add book button to hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col space-y-3 px-10 pt-20'>
-      <h1 className='text-4xl font-extrabold text-sky-500'>
-        Love your car <br />
-        like it's brand
-        <br />
-        new again.
-      </h1>
-      <h2 className='text-lg font-bold text-zinc-400'>
-        We provide high quality mobile
-        <br />
-        car detailing for a fraction of the
-        <br />
-        price.
-      </h2>
+    <main className='min-v-screen align-center flex min-h-screen flex-col gap-4 px-10 pt-20'>
+      <div className='flex w-min flex-col gap-2 sm:gap-4'>
+        <h1 className='whitespace-nowrap text-4xl font-extrabold tracking-wide text-zinc-50 sm:text-6xl'>
+          Love your car <br />
+          like it's brand
+          <br />
+          new again.
+        </h1>
+        <h2 className='whitespace-nowrap text-lg font-bold text-zinc-400 sm:text-xl'>
+          We provide high quality mobile
+          <br />
+          car detailing for a fraction of the
+          <br />
+          price.
+        </h2>
+        <Link href='/contact' className='mt-3 w-min whitespace-nowrap rounded bg-sky-500 px-2 py-1 text-xl font-bold'>
+          Book Your Premium Detail
+        </Link>
+      </div>
     </main>
   )
 }
