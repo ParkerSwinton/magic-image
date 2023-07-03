2346ff495f704037521a1f094ab2a8b2008f8ee3
ParkerSwinton <parkerdswinton@gmail.com>
2023-07-03 16:48:06 -0700

    Home screen (non-mobile)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className='min-v-screen align-center flex min-h-screen flex-col gap-4 px-10 pt-20'>
+    <main className='min-v-screen align-center flex min-h-screen flex-col items-center justify-center gap-4 px-10  sm:flex-row sm:gap-12'>
       <div className='flex w-min flex-col gap-2 sm:gap-4'>
         <h1 className='whitespace-nowrap text-4xl font-extrabold tracking-wide text-zinc-50 sm:text-6xl'>
           Love your car <br />
@@ -21,6 +21,9 @@ export default function Home() {
         <Link href='/contact' className='mt-3 w-min whitespace-nowrap rounded bg-sky-500 px-2 py-1 text-xl font-bold'>
           Book Your Premium Detail
         </Link>
+      </div>
+      <div className='h-64 w-64 sm:h-96 sm:w-96'>
+        <Image src='/temp.webp' width={1200} height={1200} alt='Car' className='h-full w-full rounded object-cover' />
       </div>
     </main>
   )
